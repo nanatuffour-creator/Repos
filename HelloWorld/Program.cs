ff71fb59c96c165db0c90efe9699701b81f14503
nanayaw246-max <johntuffourannan@gmail.com>
2025-10-15 16:21:23 +0000

    Add initial project files and implement time counter functionality

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿//Time Counter.
+Console.WriteLine("Time Counter");
+Console.Write("Enter hours : ");
+var hour = Console.ReadLine();
+var parsed = int.TryParse(hour, out int hours);
+Console.Write("Enter minutes : ");
+var minute = Console.ReadLine();
+parsed = int.TryParse(minute, out int minutes);
+Console.Write("Enter seconds : ");
+var second = Console.ReadLine();
+parsed = int.TryParse(second, out int seconds);
+
+TimeSpan time = new TimeSpan(hours, minutes, seconds);
+
+while(time.TotalSeconds >= 0)
+{
+    Console.Write($"\rTime left : {time:hh\\:mm\\:ss}");
+    time = time.Subtract(TimeSpan.FromSeconds(1));
+    await Task.Delay(1000);
+}
+Console.WriteLine("\nTime up");
